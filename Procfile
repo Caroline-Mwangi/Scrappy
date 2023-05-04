4e32fc2f79310a56d1ed919e507f3a108c9d3475
CarolineMwangi <cwmwangi50@gmail.com>
2023-05-04 16:52:31 +0300

    heroku configuration

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn authSys.wsgi --log-file -
--- scrappy/settings.py
+++ scrappy/settings.py
@@ -134,3 +134,6 @@ STATICFILES_DIRS = [BASE_DIR/'static']
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
